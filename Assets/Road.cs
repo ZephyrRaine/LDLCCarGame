769ea30aad19af8788e24b8554b4fa95b2334540
ZephyrRaine <zephyr.raine@gmail.com>
2019-04-03 00:21:56 +0200

    yeah'

---
+++ Assets/Road.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Road : MonoBehaviour
+{
+    public List<GameObject> carPrefabs;
+    public CarController playerCar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SpawnCar", 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerCar.transform.position.z >= 100f)
+        {
+            foreach(Transform t in transform)
+            {
+                t.position -= Vector3.forward * 100f;
+            }
+        }
+    }
+
+    void SpawnCar()
+    {
+            GameObject original = carPrefabs[Random.Range(0,carPrefabs.Count)];
+            Vector3 position = playerCar.transform.position;
+            position.z += 150f;
+            position.x = Random.Range(-1,2)*3f;
+            GameObject instance = Instantiate(original, position, Quaternion.identity);
+            instance.transform.SetParent(transform);
+            instance.GetComponent<Car>().speed = Random.Range(10f,50f);
+            Invoke("SpawnCar", Random.Range(0.5f,1.25f));
+    }
+}
